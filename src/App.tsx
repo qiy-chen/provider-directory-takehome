de0eefc367fe5e97623d7cc857d97834062bf013
qiy-chen <92070091+qiy-chen@users.noreply.github.com>
2025-10-15 20:49:06 -0400

    feat: added typescript

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { fetchProviders, fetchProvider } from "./api";
+import ProvidersPage from "./pages/ProvidersPage";
+import { PartialProvider } from "./types";
+
+function App() {
+  // Samples of API requests
+  const [providers, setProviders] = React.useState<PartialProvider[]>([]);
+  React.useEffect(() => {
+    fetchProviders().then((providers: PartialProvider[]) => {
+      console.log(providers);
+      setProviders({ ...providers });
+    });
+    fetchProvider("1").then(console.log);
+  }, []);
+
+  return <ProvidersPage providers={providers} />;
+}
+
+export default App;
